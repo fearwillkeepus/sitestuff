ab6be842f51480275053c2b4200a5b6f601ec1d3
fearwillkeepus <35917462+fearwillkeepus@users.noreply.github.com>
2021-10-04 17:11:43 -0700

    Update typing.js

--- typing.js
+++ typing.js
@@ -1,10 +1,10 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["responsive", "elegant", "modern", "stunning", "functional"];
+const textArray = ["responsive,", "elegant,", "modern,", "stunning,", "functional,"];
 const typingDelay = 175;
 const erasingDelay = 75;
-const newTextDelay = 1750; // Delay between current and next text
+const newTextDelay = 1500; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
 
